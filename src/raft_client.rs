1d9218a83318d2d821316fcc12ca8861ba74c5b5
Ryan Thomas <ryan@ryant.org>
2017-08-17 11:39:11 +0100

    Add in some client skeleton code.

---
+++ src/raft_client.rs
@@ -0,0 +1,31 @@
+use std::net::SocketAddr;
+
+pub struct RaftClient {
+    last_known_leader: Option<SocketAddr>,
+    all_nodes: Vec<SocketAddr>,
+}
+
+impl RaftClient {
+    /// This will construct a new `RaftClient` by connecting to the given
+    /// `SocketAddr` and populating a list of all nodes, by making an
+    /// initial call to that node.
+    pub fn new(initial_node: SocketAddr) -> RaftClient {
+        let mut client = RaftClient {
+            last_known_leader: Some(initial_node),
+            all_nodes: Vec::new(),
+        };
+
+        // TODO - make a call to the last_known_leader to determine current leader and server list.
+        Self::update_nodes(&mut client);
+        client
+    }
+
+    /// This method will attempt to make a call to the last_known_leader to determine the current leader
+    /// and node list. If this call fails or times out, then a call will be made to each of the items in the
+    /// all_nodes vector. If this is empty, or they also all time out, then the method will fail.
+    ///
+    /// This means that in the initial state, a valid node must be passed in so that the client can discover the leader.
+    fn update_nodes(&mut self) {
+
+    }
+}
